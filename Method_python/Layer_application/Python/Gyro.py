a4215572a3973f8f87d2f62da78969fd53b18ab8
Mark-cyber-cmd <dingyiming19991203@163.com>
2021-08-21 02:37:21 +0800

    建立Gyro类

--- Method_python/Layer_application/Python/Gyro.py
+++ Method_python/Layer_application/Python/Gyro.py
@@ -2,118 +2,125 @@ import keyboard
 import math
 import struct
 import time
-import Control as control
-import Servo as servo
-"""陀螺仪参数设置"""
-command_setup_open = b'\xFF\xAA\x69\x88\xB5'
-command_setup_zero = b'\xFF\xAA\x01\x08\x00'
-command_setup_close = b'\xFF\xAA\x00\x00\x00'
-command_setup_speed_100Hz = b'\xFF\xAA\x03\x09\x00'
-command_setup_data_angel = b'\xFF\xAA\x02\x08\x00'
-command_setup_data_all = b'\xFF\xAA\x02\x0A\x00'
 
-"""陀螺仪数据"""
-gyro_data = {'temper1': 0, 'roll1': 0, 'pitch1': 0, 'yaw1': 0, 'ax1': 0, 'ay1': 0, 'az1': 0, 'fps1': 0,
-             'temper2': 0, 'roll2': 0, 'pitch2': 0, 'yaw2': 0, 'ax2': 0, 'ay2': 0, 'az2': 0, 'fps2': 0,
-             'temper3': 0, 'roll3': 0, 'pitch3': 0, 'yaw3': 0, 'ax3': 0, 'ay3': 0, 'az3': 0, 'fps3': 0,
-             'temper4': 0, 'roll4': 0, 'pitch4': 0, 'yaw4': 0, 'ax4': 0, 'ay4': 0, 'az4': 0, 'fps4': 0}
-gyro_data_before = {'temper1': 0, 'roll1': 0, 'pitch1': 0, 'yaw1': 0, 'ax1': 0, 'ay1': 0, 'az1': 0, 'fps1': 0,
-                    'temper2': 0, 'roll2': 0, 'pitch2': 0, 'yaw2': 0, 'ax2': 0, 'ay2': 0, 'az2': 0, 'fps2': 0,
-                    'temper3': 0, 'roll3': 0, 'pitch3': 0, 'yaw3': 0, 'ax3': 0, 'ay3': 0, 'az3': 0, 'fps3': 0,
-                    'temper4': 0, 'roll4': 0, 'pitch4': 0, 'yaw4': 0, 'ax4': 0, 'ay4': 0, 'az4': 0, 'fps4': 0}
-"""绘图参数"""
-gyro_address = r".\Data\Data_tmp"
-servo_address = r".\Data\Data_tmp"
-gravity_flag = 0
+class Gyro:
+    """TODO 所有陀螺仪的基类"""
 
+    """陀螺仪参数设置"""
+    setup_open = b'\xFF\xAA\x69\x88\xB5'
+    setup_zero = b'\xFF\xAA\x01\x08\x00'
+    setup_close = b'\xFF\xAA\x00\x00\x00'
+    setup_speed_100Hz = b'\xFF\xAA\x03\x09\x00'
+    setup_data_angel = b'\xFF\xAA\x02\x08\x00'  # 设置只要角度
+    setup_data_all = b'\xFF\xAA\x02\x0A\x00'  # 设置数据全部输出
 
-def gyro_thread(gyro_client, gyro_addr):
-    global gravity_flag
-    if gyro_addr[0] == '192.168.43.157' or gyro_addr[0] == '192.168.43.52' \
-            or gyro_addr[0] == '192.168.43.28' or gyro_addr[0] == '192.168.43.38':
-        gyro_client.send(command_setup_open)
+    """数据存放路径"""
+    gyro_address = r".\Data\Data_tmp"
+    servo_address = r".\Data\Data_tmp"
+
+    """预设的WIFI白名单"""
+    white_list = ['192.168.43.157', '192.168.43.52', '192.168.43.28', '192.168.43.38']
+    def __init__(self, client, addr):
+        self.name = 0
+        self.client = client
+        self.addr = addr
+
+        """陀螺仪数据"""
+        self.temper = 0
+        self.roll = 0
+        self.pitch = 0
+        self.yaw = 0
+        self.ax = 0
+        self.ay = 0
+        self.az = 0
+        self.fps = 0
+        self.time_angel = 0
+        self.time_acc = 0
+        self.time_start = 0
+
+    def calibration(self, speed, data):
+        self.addr.send(Gyro.setup_open)
         time.sleep(0.1)
-        gyro_client.send(command_setup_zero)
+        self.addr.send(Gyro.setup_zero)
         time.sleep(0.1)
-        gyro_client.send(command_setup_speed_100Hz)
+        self.addr.send(speed)
         time.sleep(0.1)
-        gyro_client.send(command_setup_data_all)
+        self.addr.send(data)
         time.sleep(0.1)
-        gyro_client.send(command_setup_close)
+        self.addr.send(Gyro.setup_close)
         time.sleep(0.1)
-        raw_data = gyro_client.recv(11)
-        client_id = raw_data[2]
-        control.client_index['gyro_' + str(client_id)] = gyro_client
-        control.client_status['gyro_' + str(client_id)] = True
-        print("陀螺仪ID:", client_id, '成功连接并校准')
-        time_before = time.time()
-        time_start = time.time()
-        fps = 0
-        while True:
-            try:
-                raw_data = gyro_client.recv(11)
-                gyro_data_before['roll' + str(client_id)] = gyro_data['roll' + str(client_id)]
-                gyro_data_before['pitch' + str(client_id)] = gyro_data['roll' + str(client_id)]
-                gyro_data_before['yaw' + str(client_id)] = gyro_data['roll' + str(client_id)]
-                gravity_flag = key_scan(gravity_flag)
-                if client_id == 1:
-                    with open(gyro_address + r"\human_status.txt", "a") as f_hs:
-                        f_hs.write(str(gravity_flag))
-                        f_hs.write(" ")
-                        gravity_flag = 0
+
+    def refresh(self, raw_data):
+        if raw_data[1] == 83:
+            self.roll = struct.unpack('h', raw_data[2:4])[0] / 32768 * 180
+            self.pitch = struct.unpack('h', raw_data[4:6])[0] / 32768 * 180
+            self.yaw = struct.unpack('h', raw_data[6:8])[0] / 32768 * 180
+            self.time_angel = time.time()
+
+    def fps(self):
+        if (time.time() - self.time_start) % 1 == 0:
+            print('fps' + str(self.name) + ": ", self.fps)
+            self.fps = 0
+        else:
+            self.fps += 1
+
+    def record(self, raw_data):
+        if raw_data[1] == 83:
+            with open(Gyro.gyro_address + r"\gyro_roll" + str(self.name) + ".txt", "a") as f_gyro_roll:
+                f_gyro_roll.write(str(self.roll))
+                f_gyro_roll.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_pitch" + str(self.name) + ".txt", "a") as f_gyro_pitch:
+                f_gyro_pitch.write(str(self.pitch))
+                f_gyro_pitch.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_yaw" + str(self.name) + ".txt", "a") as f_gyro_yaw:
+                f_gyro_yaw.write(str(self.yaw))
+                f_gyro_yaw.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_angel_time" + str(self.name) + ".txt",
+                      "a") as f_angel_time:
+                f_angel_time.write(str(self.time_angel - self.time_start))
+                f_angel_time.write(" ")
+
+        if raw_data[1] == 81:
+            with open(Gyro.gyro_address + r"\gyro_ax" + str(self.name) + ".txt", "a") as f_gyro_ax:
+                f_gyro_ax.write(str(self.ax))
+                f_gyro_ax.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_ay" + str(self.name) + ".txt", "a") as f_gyro_yx:
+                f_gyro_yx.write(str(self.ay))
+                f_gyro_yx.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_az" + str(self.name) + ".txt", "a") as f_gyro_zx:
+                f_gyro_zx.write(str(self.az))
+                f_gyro_zx.write(" ")
+            with open(Gyro.gyro_address + r"\gyro_a_time" + str(self.name) + ".txt", "a") as f_a_time:
+                f_a_time.write(str(self.time_acc - self.time_start))
+                f_a_time.write(" ")
+
+        # if self.name == 1:
+        #     with open(gyro_address + r"\human_status.txt", "a") as f_hs:
+        #         f_hs.write(str(gravity_flag))
+        #         f_hs.write(" ")
+        #         gravity_flag = 0
+
+    def activate(self):
+        if self.addr[0] in Gyro.white_list:
+            raw_data = self.client.recv(11)
+            self.name = raw_data[2]
+            self.time_start = time.time()
+            self.calibration(self.setup_speed_100Hz, self.setup_data_all)
+            print("陀螺仪ID:", self.name, '成功连接并校准')
+            while True:
+                raw_data = self.client.recv(11)
                 if raw_data[1] == 83:
-                    gyro_data['roll' + str(client_id)] = \
-                        struct.unpack('h', raw_data[2:4])[0] / 32768 * 180
-                    gyro_data['pitch' + str(client_id)] = \
-                        struct.unpack('h', raw_data[4:6])[0] / 32768 * 180
-                    gyro_data['yaw' + str(client_id)] = \
-                        struct.unpack('h', raw_data[6:8])[0] / 32768 * 180
-
-                    if time.time() - time_before > 1:
-                        time_before = time.time()
-                        gyro_data['fps' + str(client_id)] = fps
-                        print('fps' + str(client_id) + ": ", fps)
-                        fps = 0
-                    else:
-                        fps = fps + 1
-
-                    with open(gyro_address + r"\gyro_roll" + str(client_id) + ".txt", "a") as f_gyro_roll:
-                        f_gyro_roll.write(str(gyro_data['roll' + str(client_id)]))
-                        f_gyro_roll.write(" ")
-                    with open(gyro_address + r"\gyro_pitch" + str(client_id) + ".txt", "a") as f_gyro_pitch:
-                        f_gyro_pitch.write(str(gyro_data['pitch' + str(client_id)]))
-                        f_gyro_pitch.write(" ")
-                    with open(gyro_address + r"\gyro_yaw" + str(client_id) + ".txt", "a") as f_gyro_yaw:
-                        f_gyro_yaw.write(str(gyro_data['yaw' + str(client_id)]))
-                        f_gyro_yaw.write(" ")
-                    with open(gyro_address + r"\gyro_angel_time" + str(client_id) + ".txt",
-                              "a") as f_angel_time:
-                        f_angel_time.write(str(time.time() - time_start))
-                        f_angel_time.write(" ")
 
                 if raw_data[1] == 81:
-                    gyro_data['ax' + str(client_id)] = \
-                        struct.unpack('h', raw_data[2:4])[0] / 32768 * 16 * 9.8
-                    gyro_data['ay' + str(client_id)] = \
-                        struct.unpack('h', raw_data[4:6])[0] / 32768 * 16 * 9.8
-                    gyro_data['az' + str(client_id)] = \
-                        struct.unpack('h', raw_data[6:8])[0] / 32768 * 16 * 9.8
 
-                    with open(gyro_address + r"\gyro_ax" + str(client_id) + ".txt", "a") as f_gyro_ax:
-                        f_gyro_ax.write(str(gyro_data['ax' + str(client_id)]))
-                        f_gyro_ax.write(" ")
-                    with open(gyro_address + r"\gyro_ay" + str(client_id) + ".txt", "a") as f_gyro_yx:
-                        f_gyro_yx.write(str(gyro_data['ay' + str(client_id)]))
-                        f_gyro_yx.write(" ")
-                    with open(gyro_address + r"\gyro_az" + str(client_id) + ".txt", "a") as f_gyro_zx:
-                        f_gyro_zx.write(str(gyro_data['az' + str(client_id)]))
-                        f_gyro_zx.write(" ")
-                    with open(gyro_address + r"\gyro_a_time" + str(client_id) + ".txt", "a") as f_a_time:
-                        f_a_time.write(str(time.time() - time_start))
-                        f_a_time.write(" ")
-            except BaseException:
-                break
-    return 1
+    def key_scan(self, flag):
+        try:  # used try so that if user pressed other than the given key error will not be shown
+            if keyboard.is_pressed('space'):  # if key 'space' is pressed
+                print('记录落脚一次')
+                flag = 1
+        except BaseException:
+            pass  # if user pressed a key other than the given key the loop will break
+        return flag
 
 
 def math_test():
@@ -147,13 +154,3 @@ def math_test():
             servo.bus_data['angel'][4] = int(500 + 300 * math.sin(2 * math.pi * i / 1000))
             time.sleep(0.01)
     return
-
-
-def key_scan(flag):
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('space'):  # if key 'q' is pressed
-            print('记录落脚一次')
-            flag = 1
-    except BaseException:
-        pass  # if user pressed a key other than the given key the loop will break
-    return flag
